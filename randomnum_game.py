26d705452b0bc9984620a2b40b97226177ffa4f9
MayaP90 <maya@Mayas-Air.fios-router.home>
2021-04-06 01:59:29 -0400

    randomnum_game.py

--- randomnum_game.py
+++ randomnum_game.py
@@ -1,14 +1,14 @@
 import random
-computer=random.randint(1,10)
-guess=int(input("Try to guess the number that is the same as the computer from 1 to 10"))
-play_again = "y"
-while play_again -'y':
+computer=random.randint(1, 10) 
+play_again = 'y'
+while play_again == 'y':
+    guess=int(input("Try to guess the number that is the same as the computer from 1 to 10:"))
     if guess > 10:
         print("Your number is too high for the range ")
     elif guess < 1: 
-        print("Your number is low for the range) 
+        print("Your number is low for the range") 
     elif guess==computer: 
-        print("well done!") 
+        print("Good job your number is the same as the computer!") 
     else: 
-        print("you\'re wrong, guess again")
+        print("Sorry this is not the number, please guess again")
     play_again = input("Want to play again(y/n): ")
